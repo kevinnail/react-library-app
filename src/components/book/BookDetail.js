75341e77d56978e3050b16328b69e952feea36f6
kevinnail <fumalicious@gmail.com>
2023-01-23 17:31:58 -0800

    added link back to catalog

--- src/components/book/BookDetail.js
+++ src/components/book/BookDetail.js
@@ -1,6 +1,6 @@
 import Book from './Book';
 import { useBookDetail } from '../../hooks/useBookDetail';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 function BookDetail() {
   // const id = 1; // TODO: Use id from route
@@ -17,7 +17,12 @@ function BookDetail() {
 
   if (loading) return <h3>Loading book...</h3>;
 
-  return <Book book={book} showDetail />;
+  return (
+    <>
+      <Book book={book} showDetail />
+      <Link to={`/books`}>Back to Catalog</Link>
+    </>
+  );
 }
 
 export default BookDetail;
