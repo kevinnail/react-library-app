cfaf6c6bf65e3d2baf3fff05ccd878ab2385e7d3
kevinnail <fumalicious@gmail.com>
2023-01-24 15:36:54 -0800

    added Home component to add link to only the / page and not the books and detail page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch, useParams } from 'react-router-dom';
 import BookList from './components/book/BookList';
 import BookDetail from './components/book/BookDetail.js';
+import Home from './components/Home/Home.js';
 
 function App() {
   // TODO: Add routes to books & views
+
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <Link to={`/books/`}>Books</Link>
-
       <Switch>
         <Route path="/books/:id" component={BookDetail} />
         <Route path="/books" component={BookList} />
+        <Route path="/" component={Home} />
       </Switch>
     </main>
   );
